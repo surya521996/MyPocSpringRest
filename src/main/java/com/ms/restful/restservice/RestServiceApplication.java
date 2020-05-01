b1dd72fab2fdc6c4aa90d97786d2ba7339be316d
surya <surya1996feb@gmail.com>
2020-05-01 21:39:45 +0530

    adde change in dev branch

--- src/main/java/com/ms/restful/restservice/RestServiceApplication.java
+++ src/main/java/com/ms/restful/restservice/RestServiceApplication.java
@@ -9,6 +9,7 @@ public class RestServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
 	System.out.println("hello");
+	System.out.println("hello-dev");
 	}
 
 }
